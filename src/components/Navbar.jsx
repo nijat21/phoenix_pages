120190f910fc07835686c74411f0e1141d6fe018
Luis Gonçalves <goncalveshenrique34@gmail.com>
2023-12-18 21:56:29 +0000

    folder fix1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link to={'/'}>
+        <h2>Home</h2>
+      </Link>
+
+      <Link>
+        <h2>Categories</h2>
+      </Link>
+      <Link>
+        <h2>Lists</h2>
+      </Link>
+      <Link>
+        <h2>About Us</h2>
+      </Link>
+
+      <form action=''>
+        <input type='text' placeholder='seacrh' />
+      </form>
+
+      <Link>
+        <h2>Log In</h2>
+      </Link>
+    </div>
+  );
 }
 
 export default Navbar;
