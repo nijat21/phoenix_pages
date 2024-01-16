67a231feaebf91fbea694f4cafc40b6f4fc6d242
Luis Gonçalves <goncalveshenrique34@gmail.com>
2024-01-16 20:58:47 +0000

    list of books

--- src/pages/MyBooks.jsx
+++ src/pages/MyBooks.jsx
@@ -3,29 +3,80 @@ import { useState, useEffect, useRef, useContext } from 'react';
 import AuthContext from '../context/AuthProvider';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import RatingDisplay from '../components/RatingDisplay';
 
 const API_URL = 'https://server-phoenix-pages.adaptable.app';
 const USERID = 1;
 
 function MyBooks() {
   const [myListOfBooks, setMyListOfBooks] = useState(null);
+  const [additionalBookInfo, setAdditionalBookInfo] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const getList = async v => {
-    if ((v = 1)) {
-      const response = await axios.get(
-        `${API_URL}/users/${USERID}?_embed=books_to_read`
-      );
+    try {
+      if (v === 1) {
+        const response = await axios.get(
+          `${API_URL}/users/${USERID}?_embed=books_to_read`
+        );
 
-      setMyListOfBooks(response.data.books_to_read);
-    } else if ((v = 2)) {
-      const response = await axios.get(
-        `${API_URL}/users/${USERID}?_embed=books_already_read`
-      );
+        setMyListOfBooks(response.data.books_to_read);
+      } else if (v === 2) {
+        const response = await axios.get(
+          `${API_URL}/users/${USERID}?_embed=books_already_read`
+        );
 
-      setMyListOfBooks(response.data.books_already_read);
+        setMyListOfBooks(response.data.books_already_read);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
+
+  const getBooksByKey = async bookKey => {
+    try {
+      const response = await axios.get(
+        `https://openlibrary.org/works/${bookKey}.json`
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getAuthorByKey = async authorKey => {
+    try {
+      const response = await axios.get(
+        `https://openlibrary.org${authorKey}.json`
+      );
+      return response.data.name;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchAdditionalInfo = async () => {
+      try {
+        const promises = myListOfBooks.map(async book => {
+          const info = await getBooksByKey(book.bookKey);
+
+          return info;
+        });
+        console.log(promises);
+
+        const promisesArray = await Promise.all(promises);
+        setAdditionalBookInfo(promisesArray);
+        console.log(additionalBookInfo);
+      } catch (error) {
+        console.error('Error fetching additional book info:', error);
+      }
+    };
+
+    if (myListOfBooks) {
+      fetchAdditionalInfo();
+    }
+  }, [myListOfBooks]);
 
   return (
     <div className='pt-14'>
@@ -35,17 +86,17 @@ function MyBooks() {
 
       <button onClick={() => getList(2)}>Already Read</button>
 
-      {myListOfBooks &&
-        myListOfBooks.map(book => {
+      {additionalBookInfo &&
+        additionalBookInfo.map(book => {
           return (
-            <>
-              <Link key={book.bookKey} to={`/books/works/${book.bookKey}`}>
+            book && (
+              <Link key={book.key} to={`/books${book.key}`}>
                 <div className='flex flex-col text-center items-center mt-16'>
-                  {book && book.cover_i && (
+                  {book && book.covers && (
                     <>
                       <div className='h-64 flex justify-center items-center'>
                         <img
-                          src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+                          src={`https://covers.openlibrary.org/b/id/${book.covers[0]}-M.jpg`}
                           alt='cover'
                           className='text-center object-cover h-60 w-26 '
                         />
@@ -54,17 +105,17 @@ function MyBooks() {
                         <div className='mh-12 flex justify-center items-center'>
                           <h2>
                             <strong>{book.title}</strong> (
-                            {book.first_publish_year})
+                            {/* {book.first_publish_year} */})
                           </h2>
                         </div>
                         <div className='mh-10 flex justify-center items-center'>
-                          <h4>{book.author_name[0]}</h4>
+                          {/* <h4>{getAuthorByKey(book.authors[0].author.key)}</h4> */}
                         </div>
                         <div className='mh-6 flex justify-center items-center'>
                           <div className='flex'>
-                            <RatingDisplay
+                            {/* <RatingDisplay
                               rating={book.ratings_average.toFixed(1)}
-                            />
+                            /> */}
                           </div>
                         </div>
                       </div>
@@ -72,7 +123,7 @@ function MyBooks() {
                   )}
                 </div>
               </Link>
-            </>
+            )
           );
         })}
     </div>
