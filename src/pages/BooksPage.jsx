e1bba092eb74e3b9d2792dcd8f755c94cbaaaed9
Luis Gonçalves <goncalveshenrique34@gmail.com>
2024-01-22 20:15:00 +0000

    merge solved

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -12,7 +12,7 @@ function BooksPage() {
   const [imageLoaded, setImageLoaded] = useState(false);
   const [booksCount, setBooksCount] = useState(5);
   const [discoverClicked, setDiscoverClicked] = useState(false);
-  const { category, setCategory } = useContext(UserContext)
+  const { category, setCategory } = useContext(UserContext);
 
   const getBooksByCategory = async subj => {
     try {
@@ -46,22 +46,21 @@ function BooksPage() {
   const getTopBooks = (input, n) => {
     const five = input
       ? input
-        .filter(book => book.readinglog_count > 300)
-        .sort(
-          (a, b) =>
-            b.ratings_average * 0.5 +
-            (b.already_read_count /
-              (b.readinglog_count - b.currently_reading_count)) *
-            5 *
-            0.5 -
-            (a.ratings_average * 0.5 +
-              (a.already_read_count /
-                (a.readinglog_count - a.currently_reading_count)) *
-              5 *
-              0.5)
-        )
-      :
-      [];
+          .filter(book => book.readinglog_count > 300)
+          .sort(
+            (a, b) =>
+              b.ratings_average * 0.5 +
+              (b.already_read_count /
+                (b.readinglog_count - b.currently_reading_count)) *
+                5 *
+                0.5 -
+              (a.ratings_average * 0.5 +
+                (a.already_read_count /
+                  (a.readinglog_count - a.currently_reading_count)) *
+                  5 *
+                  0.5)
+          )
+      : [];
     return five.slice(0, n);
   };
   const topBooks = getTopBooks(books, booksCount);
@@ -69,12 +68,12 @@ function BooksPage() {
   const handleDiscoverMore = () => {
     setDiscoverClicked(true);
     setBooksCount(booksCount + 5);
-  }
+  };
 
   const handleSeeLess = () => {
     setDiscoverClicked(false);
     setBooksCount(5);
-  }
+  };
 
   return (
     <>
@@ -84,13 +83,21 @@ function BooksPage() {
         <div className='mb-10'>
           <div className='py-5 flex flex-row justify-center items-center text-lg'>
             <button
-              className={`m-3 py-1 px-3  ${category === genericSubject ? 'border-b-2 border-black' : 'border-b-2 border-transparent'} hover:border-b-2 hover:border-black`}
+              className={`m-3 py-1 px-3  ${
+                category === genericSubject
+                  ? 'border-b-2 border-black'
+                  : 'border-b-2 border-transparent'
+              } hover:border-b-2 hover:border-black`}
               onClick={() => getBooksByCategory(genericSubject)}
             >
               General
             </button>
             <button
-              className={`m-3 py-1 px-3 ${category === 'science' ? 'border-b-2 border-black' : 'border-b-2 border-transparent'} hover:border-b-2 hover:border-black`}
+              className={`m-3 py-1 px-3 ${
+                category === 'science'
+                  ? 'border-b-2 border-black'
+                  : 'border-b-2 border-transparent'
+              } hover:border-b-2 hover:border-black`}
               onClick={() => {
                 getBooksByCategory('science');
               }}
@@ -98,34 +105,46 @@ function BooksPage() {
               Science
             </button>
             <button
-              className={`m-3 py-1 px-3 ${category === 'crime' ? 'border-b-2 border-black' : 'border-b-2 border-transparent'} hover:border-b-2 hover:border-black`}
+              className={`m-3 py-1 px-3 ${
+                category === 'crime'
+                  ? 'border-b-2 border-black'
+                  : 'border-b-2 border-transparent'
+              } hover:border-b-2 hover:border-black`}
               onClick={() => getBooksByCategory('crime')}
             >
               Crime
             </button>
             <button
-              className={`m-3 py-1 px-3 ${category === 'selfhelp' ? 'border-b-2 border-black' : 'border-b-2 border-transparent'} hover:border-b-2 hover:border-black`}
+              className={`m-3 py-1 px-3 ${
+                category === 'selfhelp'
+                  ? 'border-b-2 border-black'
+                  : 'border-b-2 border-transparent'
+              } hover:border-b-2 hover:border-black`}
               onClick={() => getBooksByCategory('selfhelp')}
             >
               Self-help
             </button>
             <button
-              className={`m-3 py-1 px-3 ${category === 'poetry&subject:drama' ? 'border-b-2 border-black' : 'border-b-2 border-transparent'} hover:border-b-2 hover:border-black`}
+              className={`m-3 py-1 px-3 ${
+                category === 'poetry&subject:drama'
+                  ? 'border-b-2 border-black'
+                  : 'border-b-2 border-transparent'
+              } hover:border-b-2 hover:border-black`}
               onClick={() => getBooksByCategory('poetry&subject:drama')}
             >
               Poetry and Drama
             </button>
           </div>
 
-          <div className='flex flex-col items-center justify-center mx-16'>
-            <section className='grid grid-cols-5 gap-x-24 gap-y-8'>
+          <div className='flex flex-col mx-16'>
+            <section className='grid grid-cols-5 gap-x-5 gap-y-8 auto-rows-auto'>
               {books &&
                 topBooks.map(book => {
                   return (
                     <Link
                       key={book.key}
                       to={`/books${book.key}`}
-                      className='min-h-max flex-shrink-0 rounded-br-lg shadow-slate-400 shadow-sm  border-2 border-slate-300 hover:border-slate-700'
+                      className='min-h-max pb-2 px-1 flex-shrink-0 rounded-br-lg shadow-slate-400 shadow-sm  border-2 border-slate-300 hover:border-slate-700'
                     >
                       <BookCard
                         book={book}
@@ -136,17 +155,21 @@ function BooksPage() {
                   );
                 })}
             </section>
-            <div className='mt-12 flex justify-center'>
-              <button className='w-40 border-solid border-2 border-amber-800 p-3 ml-4 hover:bg-amber-800 hover:text-white text-xl'
-                onClick={handleDiscoverMore}>
+            <div className='mt-12  flex justify-center'>
+              <button
+                className='w-40 p-2 mx-2 rounded-2xl  shadow-slate-400 shadow-sm border-2 text-white border-lime-700 bg-lime-700 hover:bg-lime-600 hover:border-lime-600 text-xl'
+                onClick={handleDiscoverMore}
+              >
                 Discover More
               </button>
-              {discoverClicked &&
-                <button className='w-40 border-solid border-2 border-amber-800 p-3 ml-4 hover:bg-amber-800 hover:text-white text-xl'
-                  onClick={handleSeeLess}>
+              {discoverClicked && (
+                <button
+                  className='w-40 p-2 mx-2 rounded-2xl  shadow-slate-400 shadow-sm border-2 text-white border-lime-700 bg-lime-700 hover:bg-lime-600 hover:border-lime-600 text-xl'
+                  onClick={handleSeeLess}
+                >
                   See Less
                 </button>
-              }
+              )}
             </div>
           </div>
         </div>
