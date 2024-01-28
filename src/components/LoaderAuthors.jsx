1d2aed37d42cdd60a5688d33e1c4689890145c2d
Luis Gonçalves <goncalveshenrique34@gmail.com>
2024-01-28 22:53:14 +0000

    image try

--- src/components/LoaderAuthors.jsx
+++ src/components/LoaderAuthors.jsx
@@ -2,7 +2,7 @@ function LoaderAuthors() {
   return (
     <div className='flex items-center justify-center w-screen'>
       <img
-        src='/src/assets/loader.gif'
+        src='../src/assets/loader.gif'
         alt=''
         className='w-1/6 max-h-min flex-shrink-0 flex '
       />
