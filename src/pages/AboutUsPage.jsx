8aaab2ed4ab01e8e9a6c6b0c68a578835e57610f
Luis Gonçalves <goncalveshenrique34@gmail.com>
2024-01-29 00:03:36 +0000

    aboutUs

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -3,34 +3,35 @@ import React from 'react';
 function AboutUsPage() {
   const person1 = {
     name: 'Nijat Ismayilov',
-    role: 'Co-founder',
-    bio: '',
+    role: 'Web Developer',
+    bio: 'MSc in Entrepreneurship and Innovation',
     linkedIn: '/nijat-ismayilov',
     github: '/nijat21',
   };
 
   const person2 = {
     name: 'Luís Gonçalves',
-    role: 'Co-founder',
-    bio: '',
+    role: 'Web Developer',
+    bio: 'MSc. Aerospace Engineering',
     linkedIn: '/luis-dearaujo-goncalves',
     github: '/luigoncalves',
   };
   return (
-    <div className='my-10 flex justify-evenly'>
-      <section className='w-1/4 flex flex-col px-12 py-10 items-center justify-center border-2 border-neutral-800 rounded-md shadow-sm shadow-slate-500'>
+    <div className=' flex justify-center items-center'>
+      <section className=' flex flex-col px-12 py-10 items-center justify-center '>
         <div className='flex flex-col items-center w-64 py-6 my-4 px-8 border-2 border-lime-700 rounded-xl'>
           <img
-            src='./authorGeneric.png'
+            src='./NijatPhoto.jpeg'
             alt='Nijat'
-            className=' rounded-xl w-16 h-auto mb-4'
+            className=' rounded-xl w-40 h-48 mb-4'
           />
           <h1 className='text-center text-2xl'>
             <strong>{person1.name}</strong>
           </h1>
           <h2 className='text-center text-sm'>{person1.role}</h2>
+          <p className='text-center text-sm mt-4'>{person1.bio}</p>
         </div>
-        <div className='flex flex-col items-center w-64 py-6 my-4 px-8 border-2 border-lime-600 rounded-xl'>
+        <div className='flex items-center w-64 py-6 my-4 px-8 justify-evenly border-2 border-lime-600 rounded-xl'>
           <a
             href='https://linkedin.com/in/nijat-ismayilov'
             target='_blank'
@@ -42,9 +43,6 @@ function AboutUsPage() {
               className='text-center rounded-xl w-16 h-auto mb-4'
             />
           </a>
-          <h2 className='text-center'>{person1.linkedIn}</h2>
-        </div>
-        <div className='flex flex-col items-center w-64 py-6 my-4 px-8 border-2 border-lime-500 rounded-xl'>
           <a
             href='https://github.com/nijat21'
             target='_blank'
@@ -56,23 +54,23 @@ function AboutUsPage() {
               className='text-center rounded-xl w-16 h-auto mb-4'
             />
           </a>
-          <h2 className='text-center'>{person1.github}</h2>
         </div>
       </section>
 
-      <section className='w-1/4 flex flex-col px-12 py-10 items-center justify-center border-2 border-neutral-800 rounded-md shadow-sm shadow-slate-500'>
-        <div className='flex flex-col items-center w-64 py-6 my-4 px-8 border-2 border-lime-500 rounded-xl'>
+      <section className='w-1/4 flex flex-col px-12 py-10 items-center justify-center '>
+        <div className='flex flex-col items-center w-64 py-6 my-4 px-8 border-2 border-lime-700 rounded-xl'>
           <img
-            src='./authorGeneric.png'
+            src='./LuisPhoto.jpeg'
             alt='Luís'
-            className='text-center rounded-xl w-16 h-auto mb-4'
+            className='text-center rounded-xl w-40 h-48 mb-4'
           />
           <h1 className='text-center text-2xl'>
             <strong>{person2.name}</strong>
           </h1>
           <h2 className='text-center text-sm'>{person2.role}</h2>
+          <p className='text-center text-sm mt-4'>{person1.bio}</p>
         </div>
-        <div className='flex flex-col items-center w-64 py-6 my-4 px-8 border-2 border-lime-600 rounded-xl'>
+        <div className='flex items-center w-64 py-6 my-4 px-8 justify-evenly border-2 border-lime-600 rounded-xl'>
           <a
             href='https://linkedin.com/in/luis-dearaujo-goncalves'
             target='_blank'
@@ -84,9 +82,6 @@ function AboutUsPage() {
               className='text-center rounded-xl w-16 h-auto mb-4'
             />
           </a>
-          <h2 className='text-center'>{person2.linkedIn}</h2>
-        </div>
-        <div className='flex flex-col items-center w-64 py-6 my-4 px-8 border-2 border-lime-700 rounded-xl'>
           <a
             href='https://github.com/luigoncalves'
             target='_blank'
@@ -98,7 +93,6 @@ function AboutUsPage() {
               className='text-center rounded-xl w-16 h-auto mb-4'
             />
           </a>
-          <h2 className='text-center'>{person2.github}</h2>
         </div>
       </section>
     </div>
