0334c2437d977a799c645e0b79f3ae009dbbd2b2
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-10-17 01:31:23 +0100

    Readme update

--- README.md
+++ README.md
@@ -1,81 +1,65 @@
 # Phoenix Pages
 
+![pp_image](./public/pp.jpeg)
+
 ## Description
 
-Phoenix Pages is a WebApp that allows users to browse books and add them into their Want to Read or Already Read lists. This way, it helps users to track their reading journey.
+Phoenix Pages is a web app that allows users to manage their reading lists by marking books they want to read and those they've already read. It utilizes the Open Library API to fetch book data and provides a user-friendly interface for managing book lists.
 
-<br>
+## Features
 
-## Data
+- View detailed information about a specific book, including title, author, and description.
+- Add books to "Want to Read" and "Already Read" lists.
+- Remove books from these lists.
+- Responsive design with animations using Framer Motion.
+- User feedback via toast notifications for actions performed.
 
-### Fake Backend
+## Technologies Used
 
-#### User
+- React
+- React Router
+- Axios
+- Framer Motion
+- Sonner (for toast notifications)
+- Tailwind
 
-```json
-{
-  "username": String,
-  "password": String,
-  "id": Number
-}
+## Installation
 
-```
+1. Clone the repository:
 
-#### Books To Read
+   `git clone https://github.com/nijat21/phoenix_pages.git`
 
-```json
-{
-  "id": Number,
-  "bookKey": String,
-  "userId": Number
-}
+1. Navigate to the project directory:
 
-```
+   `cd book-list-app`
 
-#### Books Already Read
+1. Install dependencies:
 
-```json
-{
-  "id": Number,
-  "bookKey": String,
-  "userId": Number
-}
+   `npm install`
 
-```
+1. Start the development server:
 
-<br>
+   `npm start`
 
-## API's
+### API Integration
 
-https://openlibrary.org/search.json
+- **Fetching Book Data**:
 
-<br>
+  - Uses Axios to make API requests to `https://openlibrary.org/works/{bookKey}.json` for book details.
+  - Fetches author details from `https://openlibrary.org/authors/{authorKey}.json`.
 
-## Packages & Libraries
+- **User Book Management**:
 
-Tailwind
-<br/>
-Chakra UI
-<br/>
-Fontawesome
-<br/>
-Framer Motion
-<br/>
-React-scroll
-<br/>
-Uuid
-<br/>
-Sonner
-<br/>
-
-<br>
+  - Functions to add/remove books from "Want to Read" and "Already Read" lists.
+  - Utilizes custom API functions: `retrieveBooksRead`, `retrieveBooksToRead`, `addBooksToRead`, `addBooksRead`, `removeBooksToRead`, and `removeBooksRead`.
 
 ## Links
 
 ### Repositories and links related to the project
 
-Github: [Frontend,](https://github.com/nijat21/phoenix_pages)
-[Fake Backend](https://github.com/luigoncalves/json-server-backend)
+GitHub: [Frontend,](https://github.com/nijat21/phoenix_pages)
+Render: [Backend](https://movies-nestjs-api.onrender.com)
+GitHub: [Backend](https://github.com/nijat21/pp_server_FastAPI-MySQL.git)
 <br/>
 
 Netlify: [Phoenix Pages](https://phoenix-pages.netlify.app/)
--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,28 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { verify } from '../API/auth.api';
-
+import React, { useState, useEffect } from "react";
+import { verify } from "../API/auth.api";
 
 const UserContext = React.createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [userLogged, setUserLogged] = useState(false);
-  const [category, setCategory] = useState('*');
-  const [searchTitle, setSearchTitle] = useState('');
-  const [enterHandler, setEnterHandler] = useState('');
+  const [category, setCategory] = useState("*");
+  const [searchTitle, setSearchTitle] = useState("");
+  const [enterHandler, setEnterHandler] = useState("");
   const [darkMode, setDarkMode] = useState(false);
-  const [theme, setTheme] = useState('');
+  const [theme, setTheme] = useState("");
   const [loading, setLoading] = useState(false);
-
 
   // Check preferred color scheme
   useEffect(() => {
-    if (window.matchMedia('(prefers-color-scheme:dark').matches) {
+    if (window.matchMedia("(prefers-color-scheme:dark").matches) {
       setDarkMode(true);
-      setTheme('dark');
+      setTheme("dark");
     } else {
       setDarkMode(false);
-      setTheme('light');
+      setTheme("light");
     }
   }, []);
 
@@ -30,29 +28,29 @@ export const UserProvider = ({ children }) => {
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
     if (darkMode) {
-      setTheme('light');
+      setTheme("light");
     } else {
-      setTheme('dark');
+      setTheme("dark");
     }
   };
 
   // Getting the user preference
   useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark');
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove("dark");
     }
   }, [theme]);
 
   // Store token
-  const storeToken = token => {
-    localStorage.setItem('authToken', token);
+  const storeToken = (token) => {
+    localStorage.setItem("authToken", token);
   };
 
   // Authentication
   const authenticateUser = async () => {
-    const storedToken = localStorage.getItem('authToken');
+    const storedToken = localStorage.getItem("authToken");
     // If userId
     if (storedToken) {
       try {
@@ -62,18 +60,19 @@ export const UserProvider = ({ children }) => {
         setUser(verified.data.User);
         setUserLogged(true);
       } catch (error) {
-        console.log('Not able to authenticate the user', error);
+        console.log("Not able to authenticate the user", error);
         clearUser();
       }
     } else {
       // If userId is not available (or is removed)
       clearUser();
     }
+    setLoading(false);
   };
 
   const clearUser = () => {
     // Upon logout, remove the id from the localStorage
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authToken");
     setUserLogged(false);
     setUser(null);
   };
@@ -87,26 +86,26 @@ export const UserProvider = ({ children }) => {
 
   useEffect(() => {
     authenticateUser();
-    // console.log("User", user);
+    console.log("User", user);
   }, []);
 
   const getTopBooks = (input, slice, readlog) => {
     const five = input
       ? input
-        .filter(book => book.readinglog_count > readlog)
-        .sort(
-          (a, b) =>
-            b.ratings_average * 0.5 +
-            (b.already_read_count /
-              (b.readinglog_count - b.currently_reading_count)) *
-            5 *
-            0.5 -
-            (a.ratings_average * 0.5 +
-              (a.already_read_count /
-                (a.readinglog_count - a.currently_reading_count)) *
-              5 *
-              0.5)
-        )
+          .filter((book) => book.readinglog_count > readlog)
+          .sort(
+            (a, b) =>
+              b.ratings_average * 0.5 +
+              (b.already_read_count /
+                (b.readinglog_count - b.currently_reading_count)) *
+                5 *
+                0.5 -
+              (a.ratings_average * 0.5 +
+                (a.already_read_count /
+                  (a.readinglog_count - a.currently_reading_count)) *
+                  5 *
+                  0.5)
+          )
       : [];
     return five.slice(0, slice);
   };
@@ -114,11 +113,28 @@ export const UserProvider = ({ children }) => {
   return (
     <UserContext.Provider
       value={{
-        user, setUser, userLogged, setUserLogged, storeToken, category, setCategory, searchTitle, setSearchTitle,
-        enterHandler, setEnterHandler, getTopBooks, darkMode, setDarkMode, toggleDarkMode, theme, setTheme, loading,
+        user,
+        setUser,
+        userLogged,
+        setUserLogged,
+        storeToken,
+        category,
+        setCategory,
+        searchTitle,
+        setSearchTitle,
+        enterHandler,
+        setEnterHandler,
+        getTopBooks,
+        darkMode,
+        setDarkMode,
+        toggleDarkMode,
+        theme,
+        setTheme,
+        loading,
         setLoading,
         // functions
-        authenticateUser, logOutUser,
+        authenticateUser,
+        logOutUser,
       }}
     >
       {children}
